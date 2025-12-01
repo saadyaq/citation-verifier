cd0979bf38a14f233f5fbf09a5da41c8b1462720
saadyaq <saadyaqine91@gmail.com>
2025-12-01 22:22:26 +0100

    Verdict class

--- src/citation_verifier/models.py
+++ src/citation_verifier/models.py
@@ -1,2 +1,12 @@
 from pydantic import BaseModel, Field 
-from enum 
+from enum import Enum
+from typing import Optional 
+
+class Verdict(str, Enum):
+    """List of potential verdicts"""
+    SUPPORTED="supported"
+    NOT_SUPPORTED="not_supported"
+    PARTIAL="partial"
+    INCONCLUSIVE="inconclusive"
+    SOURCE_UNAVAILABLE="source_unavailable"
+
